1dacd7f9356215f90035ae84889b73306a75949f
ThePhysic <ridleigh.riddler@gmail.com>
2024-06-10 14:18:31 -0700

    Todo automation + keyword implementation

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ const UNREAD_TARGET_CHANNEL_ID = process.env.UNREAD_TARGET_CHANNEL_ID;
 const TODO_TARGET_CHANNEL_ID = process.env.TODO_TARGET_CHANNEL_ID;
 const TALKING_POINT_TARGET_CHANNEL_ID = process.env.TALKING_POINT_CHANNEL_ID;
 const GENERAL_CHANNEL_ID = process.env.GENERAL_CHANNEL_ID;
+const USERNAME_REGEX = /<@(\d+)>/g;
+const TODO_REGEX = /TODO/;
+const deleteTodo = [false, false];
 
 
 // Event listener for when bot is ready:
@@ -70,6 +73,83 @@ client.on("messageReactionAdd", async (reaction, user) => {
     if (reaction.emoji.name === TODO_TARGET_EMOJI) {
         emojiReactToSend(reaction.message, user, TODO_TARGET_CHANNEL_ID);
     }
+
+    // Check emojis for deleting todo:
+    const message = reaction.message.content;
+
+    if (reaction.emoji.name === DELETE_TODO_EMOJI) {
+        if (message.match(USERNAME_REGEX)[0] === `<@${user.id}>`) {
+            deleteTodo[0] = true;
+        }
+        if (message.match(USERNAME_REGEX)[1] === `<@${user.id}>`) {
+            deleteTodo[1] = true;
+        }
+        if (deleteTodo[0] === true && deleteTodo[1] === true) {
+            reaction.message.delete();
+            deleteTodo[0] = false;
+            deleteTodo[1] = false;
+        }
+    }
+});
+
+// Event listener for reactions removed from messages:
+client.on("messageReactionRemove", async (reaction, user) => {
+    // Ignore bot reactions:
+    if (user.bot) {
+        return;
+    }
+
+    // Fetch partials if necessary:
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        }
+        catch (error) {
+            console.error("Something went wrong when fetching the reaction: ", error);
+            return;
+        }
+    }
+
+    // Check emojis removed for deleting todo:
+    const message = reaction.message.content;
+
+    if (message.match(USERNAME_REGEX)[0] === `<@${user.id}>`) {
+        deleteTodo[0] = false;
+    }
+    if (message.match(USERNAME_REGEX)[1] === `<@${user.id}>`) {
+        deleteTodo[1] = false;
+    }
+});
+
+// Event listener for messages sent:
+client.on("messageCreate", async (message) => {
+    // Skip bot's own messages:
+    if (message.author.bot) {
+        return;
+    }
+
+
+    // Check if message is a reply:
+    if (message.reference) {
+        const originalMessageId = message.reference.messageId;
+
+        try {
+            // Fetch original message being replied to:
+            const originalMessage = await message.channel.messages.fetch(originalMessageId);
+            const originalMessageLink = `https://discord.com/channels/${originalMessage.guildId}/${originalMessage.channelId}/${originalMessage.id}`;
+
+            // Add todo:
+            if (message.content.substring(0, 5).match(TODO_REGEX)) {
+                sendTodo(originalMessage, message.author, TODO_TARGET_CHANNEL_ID);
+            }
+            else if (message.content.match(TODO_REGEX)) {
+                sendTodo(message, message.author, TODO_TARGET_CHANNEL_ID);
+            }
+    // Add todo that isn't a reply:
+    else if (message.content.match(TODO_REGEX)) {
+        sendTodo(message, message.author, TODO_TARGET_CHANNEL_ID);
+    }
+    }
     }
 });
 
@@ -103,6 +183,22 @@ const emojiReactToSend = (message, user, targetChannelId, interaction = null) =>
     if (targetChannel && targetChannel.isTextBased()) {
         const messageLink = `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
         targetChannel.send(`**Message from ${message.author}**:\n**Sent over by ${user}**:\n${message.content}\n${messageLink}`);
+        if (interaction) {
+            interaction.reply(`Message pinned successfully to ${targetChannel.name}.`);
+        }
+    }
+    else {
+        console.error("Target channel not found or is not a text channel.");
+    }
+};
+
+// Function for sending todos:
+const sendTodo = (message, user, targetChannelId, interaction = null) => {
+    const targetChannel = client.channels.cache.get(targetChannelId);
+
+    if (targetChannel && targetChannel.isTextBased()) {
+        const messageLink = `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
+        targetChannel.send(`**Message from ${message.author}**:\n**Sent over by ${user}**:\n**TODO:** ${message.content}\n${messageLink}`);
         if (interaction) {
             interaction.reply(`Message pinned successfully to ${targetChannel.name}.`);
         }
