56bfbfdf905697189cde038b020149a26d2b7a57
ThePhysic <ridleigh.riddler@gmail.com>
2024-06-10 13:49:05 -0700

    Refactored const variables names and added intents

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { Client, GatewayIntentBits, Partials, MessageCollector, ChannelType, EmbedBuilder, VoiceChannel } from "discord.js";
 
 // Initialize the bot client:
 const client = new Client({
@@ -12,6 +12,7 @@ const client = new Client({
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildVoiceStates, 
     ],
     partials: [
         Partials.Message,
@@ -21,13 +22,15 @@ const client = new Client({
 });
 
 // Define target emojis and channels:
-const firstTargetEmoji = "📌";
-const secondTargetEmoji = "🔖";
-const thirdTargetEmoji = "📝";
-const deleteTodoEmoji = "✅";
-const firstTargetChannelId = process.env.TARGET_CHANNEL_ID;
-const secondTargetChannelId = process.env.SECOND_TARGET_CHANNEL_ID;
-const thirdTargetChannelId = process.env.THIRD_TARGET_CHANNEL_ID;
+const PIN_TARGET_EMOJI = "📌";
+const UNREAD_TARGET_EMOJI = "🔖";
+const TODO_TARGET_EMOJI = "📝";
+const DELETE_TODO_EMOJI = "✅";
+const PIN_TARGET_CHANNEL_ID = process.env.PIN_CHANNEL_ID;
+const UNREAD_TARGET_CHANNEL_ID = process.env.UNREAD_TARGET_CHANNEL_ID;
+const TODO_TARGET_CHANNEL_ID = process.env.TODO_TARGET_CHANNEL_ID;
+const TALKING_POINT_TARGET_CHANNEL_ID = process.env.TALKING_POINT_CHANNEL_ID;
+const GENERAL_CHANNEL_ID = process.env.GENERAL_CHANNEL_ID;
 
 
 // Event listener for when bot is ready:
@@ -54,18 +57,19 @@ client.on("messageReactionAdd", async (reaction, user) => {
     }
 
     // Check if reaction emoji matches first target emoji:
-    if (reaction.emoji.name === firstTargetEmoji) {
-        emojiReactToSend(reaction.message, user, firstTargetChannelId);
+    if (reaction.emoji.name === PIN_TARGET_EMOJI) {
+        emojiReactToSend(reaction.message, user, PIN_TARGET_CHANNEL_ID);
     }
 
     // Check if reaction emoji matches second target emoji:
-    if (reaction.emoji.name === secondTargetEmoji) {
-        emojiReactToSend(reaction.message, user, secondTargetChannelId);
+    if (reaction.emoji.name === UNREAD_TARGET_EMOJI) {
+        emojiReactToSend(reaction.message, user, UNREAD_TARGET_CHANNEL_ID);
     }
 
     // Check if reaction emoji matches third target emoji:
-    if (reaction.emoji.name === thirdTargetEmoji) {
-        emojiReactToSend(reaction.message, user, thirdTargetChannelId);
+    if (reaction.emoji.name === TODO_TARGET_EMOJI) {
+        emojiReactToSend(reaction.message, user, TODO_TARGET_CHANNEL_ID);
+    }
     }
 });
 
@@ -82,7 +86,7 @@ client.on("interactionCreate", async (interaction) => {
         const messageId = options.getString("messageid");
         try {
             const message = await interaction.channel.messages.fetch(messageId);
-            emojiReactToSend(message, interaction.user, firstTargetChannelId, interaction);
+            emojiReactToSend(message, interaction.user, PIN_TARGET_CHANNEL_ID, interaction);
         } 
         catch (error) {
             console.error("Something went wrong when fetching the message: ", error);
